a3b857db63a39cd8aa198fea915dd5bf984dfd83
matthew.lau <lau.cy.matthew@gmail.com>
2022-07-01 10:43:26 +0100

    feat: added step state to Calculate page

--- src/views/Calculate.vue
+++ src/views/Calculate.vue
@@ -81,7 +81,7 @@
               >
                 <div
                   class="bg-blue-600 h-2.5 rounded-full"
-                  style="width: 45%"
+                  :style="{ width: (currentStep / 3) * 100 + '%' }"
                 ></div>
               </div>
               <div class="mb-5">
@@ -130,16 +130,66 @@
                 ></textarea>
               </div>
               <div>
-                <button
-                  class="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
-                >
-                  Back
-                </button>
-                <button
-                  class="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
-                >
-                  Submit
-                </button>
+                <div class="flex mt-4">
+                  <button
+                    class="border border-indigo-500 text-indigo-500 block rounded-sm font-bold py-4 px-6 mr-2 flex items-center"
+                    @click="handleOnPreviousClick"
+                    :disabled="shouldDisablePreviousButton"
+                    :class="{
+                      'bg-indigo-500 hover:bg-indigo-400 hover:text-white text-white ':
+                        !shouldDisablePreviousButton,
+                    }"
+                  >
+                    <svg
+                      class="h-5 w-5 mr-2 fill-current"
+                      version="1.1"
+                      id="Layer_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                      x="0px"
+                      y="0px"
+                      viewBox="-49 141 512 512"
+                      style="enable-background: new -49 141 512 512"
+                      xml:space="preserve"
+                    >
+                      <path
+                        id="XMLID_10_"
+                        d="M438,372H36.355l72.822-72.822c9.763-9.763,9.763-25.592,0-35.355c-9.763-9.764-25.593-9.762-35.355,0 l-115.5,115.5C-46.366,384.01-49,390.369-49,397s2.634,12.989,7.322,17.678l115.5,115.5c9.763,9.762,25.593,9.763,35.355,0 c9.763-9.763,9.763-25.592,0-35.355L36.355,422H438c13.808,0,25-11.193,25-25S451.808,372,438,372z"
+                      ></path>
+                    </svg>
+                    Previous page
+                  </button>
+                  <button
+                    class="border border-indigo-500 text-indigo-500 block rounded-sm font-bold py-4 px-6 ml-2 flex items-center"
+                    @click="handleOnNextClick"
+                    :disabled="shouldDisableNextButton"
+                    :class="{
+                      'bg-indigo-500 hover:bg-indigo-400 hover:text-white text-white':
+                        !shouldDisableNextButton,
+                    }"
+                  >
+                    Next page
+                    <svg
+                      class="h-5 w-5 ml-2 fill-current"
+                      clasversion="1.1"
+                      id="Layer_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                      x="0px"
+                      y="0px"
+                      viewBox="-49 141 512 512"
+                      style="enable-background: new -49 141 512 512"
+                      xml:space="preserve"
+                    >
+                      <path
+                        id="XMLID_11_"
+                        d="M-24,422h401.645l-72.822,72.822c-9.763,9.763-9.763,25.592,0,35.355c9.763,9.764,25.593,9.762,35.355,0
+            l115.5-115.5C460.366,409.989,463,403.63,463,397s-2.634-12.989-7.322-17.678l-115.5-115.5c-9.763-9.762-25.593-9.763-35.355,0
+            c-9.763,9.763-9.763,25.592,0,35.355l72.822,72.822H-24c-13.808,0-25,11.193-25,25S-37.808,422-24,422z"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </form>
           </div>
@@ -149,3 +199,47 @@
     </div>
   </section>
 </template>
+
+<script lang="ts">
+import { defineComponent, computed, reactive, toRefs } from "vue";
+
+interface CalculateState {
+  currentStep: number;
+}
+
+export default defineComponent({
+  components: {},
+  setup() {
+    const calculateState = reactive<CalculateState>({
+      currentStep: 1,
+    });
+
+    const handleOnNextClick = () => {
+      calculateState.currentStep++;
+    };
+
+    const handleOnPreviousClick = () => {
+      calculateState.currentStep--;
+    };
+
+    const shouldDisablePreviousButton = computed(
+      () => calculateState.currentStep === 1
+    );
+
+    const shouldDisableNextButton = computed(
+      () => calculateState.currentStep === 3
+    );
+
+    return {
+      handleOnNextClick,
+      handleOnPreviousClick,
+      shouldDisablePreviousButton,
+      shouldDisableNextButton,
+      ...toRefs(calculateState),
+    };
+  },
+  onMounted() {
+    // loadUser();
+  },
+});
+</script>
