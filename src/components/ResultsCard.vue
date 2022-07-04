5b716298fee859678c19d851527e50d7dd75add2
matthew.lau <lau.cy.matthew@gmail.com>
2022-07-04 14:17:38 +0100

    feat: added ResultsCard component

---
+++ src/components/ResultsCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    class="relative flex flex-col sm:flex-row sm:items-center bg-white shadow rounded-md py-5 pl-6 pr-8 sm:pr-6"
+  >
+    <div class="bg-white p-6 md:mx-auto">
+      <svg
+        v-if="shouldRenderCheckIcon"
+        viewBox="0 0 24 24"
+        class="text-green-600 w-16 h-16 mx-auto my-6"
+      >
+        <path
+          fill="currentColor"
+          d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
+        ></path>
+      </svg>
+      <svg
+        v-if="!shouldRenderCheckIcon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="text-red-600 w-16 h-16 mx-auto my-6"
+      >
+        <path
+          fill="currentColor"
+          d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5 15.538l-3.592-3.548 3.546-3.587-1.416-1.403-3.545 3.589-3.588-3.543-1.405 1.405 3.593 3.552-3.547 3.592 1.405 1.405 3.555-3.596 3.591 3.55 1.403-1.416z"
+        />
+      </svg>
+      <div class="text-center">
+        <h3 class="md:text-2xl text-base text-gray-900 text-center">
+          {{ subject }}
+        </h3>
+        <p class="text-gray-600 my-2">
+          {{ body }}
+        </p>
+        <div class="flex flex-col items-center mt-12 text-center">
+          <span class="relative inline-flex w-full md:w-auto">
+            <a
+              href="/calculate"
+              class="inline-flex items-center justify-center w-full px-8 py-2 text-base font-bold leading-6 text-white bg-blue-600 border border-transparent rounded-full md:w-auto hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
+            >
+              Calculate again
+            </a>
+            <span
+              class="absolute top-0 right-0 px-2 py-1 -mt-3 -mr-6 text-xs font-medium leading-tight text-white bg-green-400 rounded-full"
+              >now!</span
+            >
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    subject: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    shouldRenderCheckIcon: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+</script>
--- src/views/Success.vue
+++ src/views/Success.vue
@@ -7,55 +7,11 @@
       <div
         class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center"
       >
-        <div
-          class="relative flex flex-col sm:flex-row sm:items-center bg-white shadow rounded-md py-5 pl-6 pr-8 sm:pr-6"
-        >
-          <div class="bg-white p-6 md:mx-auto">
-            <svg
-              v-if="shouldWorkRemote || calculationId"
-              viewBox="0 0 24 24"
-              class="text-green-600 w-16 h-16 mx-auto my-6"
-            >
-              <path
-                fill="currentColor"
-                d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
-              ></path>
-            </svg>
-            <svg
-              v-if="!shouldWorkRemote && !calculationId"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              class="text-red-600 w-16 h-16 mx-auto my-6"
-            >
-              <path
-                fill="currentColor"
-                d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5 15.538l-3.592-3.548 3.546-3.587-1.416-1.403-3.545 3.589-3.588-3.543-1.405 1.405 3.593 3.552-3.547 3.592 1.405 1.405 3.555-3.596 3.591 3.55 1.403-1.416z"
-              />
-            </svg>
-            <div class="text-center">
-              <h3 class="md:text-2xl text-base text-gray-900 text-center">
-                {{ subject }}
-              </h3>
-              <p class="text-gray-600 my-2">
-                {{ body }}
-              </p>
-              <div class="flex flex-col items-center mt-12 text-center">
-                <span class="relative inline-flex w-full md:w-auto">
-                  <a
-                    href="/calculate"
-                    class="inline-flex items-center justify-center w-full px-8 py-2 text-base font-bold leading-6 text-white bg-blue-600 border border-transparent rounded-full md:w-auto hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
-                  >
-                    Calculate again
-                  </a>
-                  <span
-                    class="absolute top-0 right-0 px-2 py-1 -mt-3 -mr-6 text-xs font-medium leading-tight text-white bg-green-400 rounded-full"
-                    >now!</span
-                  >
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ResultsCard
+          :subject="subject"
+          :body="body"
+          :shouldRenderCheckIcon="shouldWorkRemote || calculationId !== ''"
+        />
       </div>
       <!-- End Main Hero Content -->
     </div>
@@ -66,9 +22,10 @@
 import { defineComponent } from "vue";
 import { useResults } from "../modules/results";
 import Nav from "../components/Nav.vue";
+import ResultsCard from "../components/ResultsCard.vue";
 
 export default defineComponent({
-  components: { Nav },
+  components: { Nav, ResultsCard },
   setup() {
     const { savings, shouldWorkRemote, id } = useResults();
     const calculationId = id.value;
